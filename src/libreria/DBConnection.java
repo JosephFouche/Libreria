3b9ecfa3b5c9f7756579412e52cb222cf39e2cff
Eduardo Martinez <104235261+JosephFouche@users.noreply.github.com>
2023-12-26 15:53:12 -0300

    Add files via upload

---
+++ src/libreria/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libreria;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Eduardo
+ */
+public class DBConnection {
+    static Connection con = null;
+    
+    public static Connection getConnection(){
+    
+         try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/libreria_ms","root","");
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+        return con;
+    }
+ 
+}
+
+    
